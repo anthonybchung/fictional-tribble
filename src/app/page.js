406b1b00669a00356e3c4ee25ab610092e2171a5
anthony.b.chung <anthony.b.chung@gmail.com>
2024-03-03 20:48:31 +1100

    Body overflow control

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-quaternary">
-     
+     <section className='h-96 bg-secondary'>
+
+     </section>
+     <section className='h-96 bg-primary'></section>
     </main>
   );
 }
--- src/app/ui/Navbar/Navbar.jsx
+++ src/app/ui/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import MenuList from '../MenuList/MenuList'
 import MenuButton from '../MenuButton/MenuButton'
 
@@ -11,6 +11,16 @@ const Navbar = ({listItems, companyName}) => {
   const handleClose = ()=>{
     setClosed(prev=>!prev)
   }
+
+  useEffect(()=>{
+    if(closed){
+      document.body.classList.add('overflow-auto')
+      document.body.classList.remove('overflow-hidden')
+    }else{
+      document.body.classList.add('overflow-hidden')
+      document.body.classList.remove('overflow-auto')
+    }
+  },[closed])
 
   return (
     <nav className='h-12 w-screen px-6 bg-primary text-quaternary flex flex-row items-center justify-between'>
